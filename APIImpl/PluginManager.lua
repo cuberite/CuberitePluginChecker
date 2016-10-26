7fc921dc47127224213512288d189137707f7285
Mattes D <github@xoft.cz>
2016-10-26 11:10:27 +0200

    Implemented cPluginManager callbacks.
    Ref.: #11

--- APIImpl/PluginManager.lua
+++ APIImpl/PluginManager.lua
@@ -29,6 +29,43 @@ end
 
 
 
+--- Implementation of the cPluginManager:BindConsoleCommand function
+-- There are two separate API endpoints for the same implementation, so the implementation is pulled into a common function
+local function BindConsoleCommand(a_Simulator, a_Self, a_Command, a_Callback, a_HelpString)
+	if (a_Command:match("%s")) then
+		a_Simulator.logger:trace("Registering a console command containing whitespace(%s), such a handler will never be called by Cuberite.%s", a_Command, debug.traceback("", 2))
+	end
+	if (a_Simulator.registeredConsoleCommandHandlers[a_Command]) then
+		a_Simulator.logger:info("Registering  a console command that is already registered (%s). The second handler will not be called / tested.", a_Command)
+		return
+	end
+	a_Simulator.registeredConsoleCommandHandlers[a_Command] =
+	{
+		callback = a_Callback,
+		helpString = a_HelpString,
+	}
+	return true
+end
+
+
+
+
+
+--- Implementation of the cPluginManager:ForEachPlugin function
+-- There are two separate API endpoints for the same implementation
+local function ForEachPlugin(a_Simulator, a_Self, a_Callback)
+	local res = a_Simulator:processCallbackRequest({
+		Function = a_Callback,
+		ParamValues = { a_Self:GetCurrentPlugin() },
+		Notes = "cPluginManager:ForEachPlugin() callback",
+	})
+	return not(res == true)  -- If the callback signalized an abort, return false
+end
+
+
+
+
+
 return
 {
 	["<static> cPluginManager:AddHook(cPluginManager#PluginHook, function)"] = function (a_Simulator, a_Self, a_HookType, a_Callback)
@@ -38,7 +75,62 @@ return
 	end,
 
 	["<static> cPluginManager:BindCommand(string, string, function, string)"] = BindCommand,
+
 	["cPluginManager:BindCommand(string, string, function, string)"] = BindCommand,
+
+	["<static> cPluginManager:BindConsoleCommand(string, function, string)"] = BindConsoleCommand,
+
+	["cPluginManager:BindConsoleCommand(string, function, string)"] = BindConsoleCommand,
+
+	["<static> cPluginManager:DoWithPlugin(string, function) -> (boolean)"] = function(a_Simulator, a_Class, a_PluginName, a_Callback)
+		-- The currently tested plugin is the only one present, if the name matches, call with "self":
+		local currPlugin = cPluginManager:GetCurrentPlugin()
+		if (a_PluginName == currPlugin:GetName()) then
+			local res = a_Simulator:processCallbackRequest({
+				Function = a_Callback,
+				ParamValues = { currPlugin },
+				Notes = "cPluginManager:DoWithPlugin() callback",
+			})
+			return (res == true)  -- Convert the return value to a single bool
+		end
+
+		-- Plugin not found:
+		return false
+	end,
+
+	["cPluginManager:ForEachCommand(function) -> (boolean)"] = function(a_Simulator, a_Self, a_Callback)
+		for cmd, desc in pairs(a_Simulator.registeredCommandHandlers) do
+			local res = a_Simulator:processCallbackRequest({
+				Function = a_Callback,
+				ParamValues = { cmd, desc.permission, desc.helpString },
+				Notes = string.format("cPluginManager:ForEachCommand() callback, command %s", cmd),
+			})
+			if (res == true) then
+				a_Simulator.logger:trace("cPluginManager:ForEachCommand() callback has aborted the loop at command %s", cmd)
+				return false
+			end
+		end  -- for cmd, desc
+		return true
+	end,
+
+	["cPluginManager:ForEachConsoleCommand(function) -> (boolean)"] = function(a_Simulator, a_Self, a_Callback)
+		for cmd, desc in pairs(a_Simulator.registeredConsoleCommandHandlers) do
+			local res = a_Simulator:processCallbackRequest({
+				Function = a_Callback,
+				ParamValues = { cmd, desc.helpString },
+				Notes = string.format("cPluginManager:ForEachConsoleCommand() callback, command %s", cmd),
+			})
+			if (res == true) then
+				a_Simulator.logger:trace("cPluginManager:ForEachConsoleCommand() callback has aborted the loop at command %s", cmd)
+				return false
+			end
+		end  -- for cmd, desc
+		return true
+	end,
+
+	["<static> cPluginManager:ForEachPlugin(function) -> (boolean)"] = ForEachPlugin,
+
+	["cPluginManager:ForEachPlugin(function) -> (boolean)"] = ForEachPlugin,
 
 	["cPluginManager:GetCurrentPlugin()"] = function (a_Simulator, a_Self)
 		local res = a_Simulator:createInstance({Type = "cPluginLua"})
